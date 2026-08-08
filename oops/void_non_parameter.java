722184ec8bd2605170506babcc8feeabf1cd0413
gokularul277 <gokularul277@gmail.com>
2026-08-08 13:18:52 +0000

    deleted un wanted file

--- oops/void_non_parameter.java
+++
@@ -1,8 +0,0 @@
-public class void_non_parameter {
-    public static void main(String[] args) {
-        add();
-    }
-    public static void add(){
-        System.out.println(4+8);
-    }
-}
--- oops/void_parameter.java
+++
@@ -1,9 +0,0 @@
-public class void_parameter {
-    public static void main(String[] args) {
-        add(0,9);
-        
-    }
-    public static void add(int a,int b){
-        System.out.println(a+b);
-    }
-}
