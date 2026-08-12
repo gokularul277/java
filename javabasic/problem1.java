2c61455b07b7f5d3c578cbc0835c8399bf187842
gokularul277 <gokularul277@gmail.com>
2026-08-12 10:24:09 +0000

    vasanthfile added

---
+++ javabasic/problem1.java
@@ -0,0 +1,18 @@
+public class problem1 {
+    public static void main(String[] args) {
+        int a=99800;
+        int count=0;
+        int b=0;
+
+        while(a>0){
+            b=a%10;
+            a=a/10;
+            count++;
+
+
+        }
+        System.out.println(count);
+        System.out.println(231%10);
+        System.out.println(231/10);
+    }
+}
