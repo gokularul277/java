796a0f09665703272be0627ac1362a778dc74343
gokularul277 <gokularul277@gmail.com>
2026-08-17 17:33:10 +0000

    Threesome

--- Dsa_problems/threesum.java
+++ Dsa_problems/threesum.java
@@ -1,14 +1,15 @@
-public class two_sum {
+public class threesum {
     public static void main(String[] args) {
         int a[]={2,7,11,15};
-        int target=9;
+        int target=24;
         Two(a, target);}
 
     static void Two(int[] a,int target){
         for (int i = 0; i <a.length; i++) {
             for (int j = i+1; j<a.length; j++) {
-                if(a[i]+a[j]==target){
-                    System.out.println(i+  "  "+j);
+                for (int k = j+1; k <a.length; k++) {
+                    if(a[i]+a[j]+a[k]==target){
+                        System.out.println(i+  "  "+j+ "  "+k);
                     }
                     
                 }
@@ -16,4 +17,5 @@ public class two_sum {
             }
         }
     }
+}
 
